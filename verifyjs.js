bb7f93c03f7b12c5eed1baad346457a4c618ff9d
Mittal <mittalkumargupta@gmail.com>
2022-11-21 04:07:53 +0530

    second

--- verifyjs.js
+++ verifyjs.js
@@ -1,4 +1,41 @@
-document.querySelectorAll(".otps")[0].focus();
-document.querySelector(".otps").addEventListener("keydown",()=>{
-setTimeout(()=>{    document.querySelectorAll(".otps")[1].focus()},100)
+const d=document;
+const input=d.querySelectorAll(".otps");
+const otp_arry=new Array();
+input[0].focus();
+input.forEach((element,index)=>{
+    
+    element.addEventListener("keydown",(event)=>{
+        if(event.key>=0&&event.key<=9&&event.key!=" ")
+        {  if(input[index+1]!==undefined)
+            {  console.log(event.key);
+                otp_arry.push(event.key);
+                setTimeout(()=>{
+                    input[index+1].focus();
+                  },100)
+                
+            }
+            else
+            {
+                console.log(event.key); 
+                otp_arry.push(event.key);
+            }
+            
+        }
+
+       
+            if(event.key=="Backspace")
+            {    if(input[index-1]!=undefined)
+                setTimeout(()=>{
+                input[index-1].focus();
+            },100)
+                 
+                
+            }
+          
+
+       
+    });
+
+   
 })
+
