a3fc78da508b7ba53fe6d23afb04ae8947854cd0
Mittal <mittalkumargupta@gmail.com>
2022-11-21 05:08:36 +0530

    third 95%

--- verifyjs.js
+++ verifyjs.js
@@ -1,4 +1,5 @@
 const d=document;
+const setotp=748834;
 const input=d.querySelectorAll(".otps");
 const otp_arry=new Array();
 input[0].focus();
@@ -7,28 +8,56 @@ input.forEach((element,index)=>{
     element.addEventListener("keydown",(event)=>{
         if(event.key>=0&&event.key<=9&&event.key!=" ")
         {  if(input[index+1]!==undefined)
-            {  console.log(event.key);
+            {  
                 otp_arry.push(event.key);
                 setTimeout(()=>{
                     input[index+1].focus();
+                    input[index].classList.add("datap");
+                    
                   },100)
                 
             }
             else
             {
-                console.log(event.key); 
+                input[index].classList.add("datap");
                 otp_arry.push(event.key);
+                if(otp_arry.length===6)
+                {  let valo=otp_arry.reduce((acc,el)=>{
+                           return  acc+el.toString();
+                       },"")
+
+
+                    setTimeout(()=>{
+                        if(Number(valo)==setotp)
+                          alert("VERIFIED 🤗🤗🤗🤗🤗🤗");
+                          else
+                          {
+                            alert("INVALID OTP 😢😢😢😢😢😢");
+                          }
+                        
+                     },100)
+                }
+              
             }
             
         }
 
        
             if(event.key=="Backspace")
-            {    if(input[index-1]!=undefined)
-                setTimeout(()=>{
-                input[index-1].focus();
-            },100)
-                 
+            {    if(input[index-1]!=undefined&&input[index-1].value!="")
+                 {
+                    setTimeout(()=>{
+                        input[index-1].focus();
+                        input[index].classList.remove("datap");
+                         otp_arry.pop(); 
+                    },100)
+                         
+                 }
+                 else
+                 {
+                    input[index].classList.remove("datap");
+                    otp_arry.pop(); 
+                 }
                 
             }
           
@@ -38,4 +67,5 @@ input.forEach((element,index)=>{
 
    
 })
+
 
